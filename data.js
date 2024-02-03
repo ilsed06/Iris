5beb2f3c361f9b942f4b20a00bbca2530dbc59c0
ilsed06 <130949186+ilsed06@users.noreply.github.com>
2024-02-03 17:53:14 -0500

    Create data.js
    stores all data

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = 
+[
+    {
+        "name": "codePlant",
+        "type": "iris",
+        "level": 0,
+        "questions": [
+            {
+                "question": "1?",
+                "answer": "1"
+            },
+            {
+                "question": "2?",
+                "answer": "2"
+            }
+        ]
+    },
+    {
+        "name": "testPlant",
+        "type": "flower",
+        "level": 0,
+        "questions": [
+            {
+                "question": "1?",
+                "answer": "1"
+            }
+        ]
+    }
+]
