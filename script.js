3d9478c641379af43e6fafe53bc74dbb0e8fc46f
ilsed06 <130949186+ilsed06@users.noreply.github.com>
2024-02-03 16:03:21 -0500

    Update script.js
    colors updated, seed grid created, question pop up added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var totalLevel = 0;
+document.getElementById("name").innerHTML = name;
 
+//Fill in Name
+function getName()
+{
+  var name = prompt("What is your name?");
+  document.getElementById("name").innerHTML = name;
+}
+
+var levels = [0, 0, 0, 0, 0, 0];
+
+//code plant
+function codePlant() {
+  const questions = ["1?", "2?", "3?"];
+  prompt("Question: " + questions[0] + totalLevel);
+
+  totalLevel++;  
+}
