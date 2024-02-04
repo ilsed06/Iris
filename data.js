61555dee741c9e111c133082e2a21f4ae746b7b1
TiffWid <150554063+TiffWid@users.noreply.github.com>
2024-02-04 11:07:45 -0500

    Update data.js
    Added other questions

--- data.js
+++ data.js
@@ -1,29 +1,140 @@
-const data = 
-[
-    {
-        "name": "codePlant",
-        "type": "iris",
-        "level": 0,
-        "questions": [
+const data =
+  {
+     "codePlant":{
+        "type":"Purple Iris",
+        "title": "Coding",
+        "level":0,
+        "unlockLevel": 0,
+       "info": "Want to learn more about coding? Check these links!     Python: https://www.python.org/about/gettingstarted/,             Web-Development: https://bootcamp.berkeley.edu/resources/coding/learn-web-development,       HTML/CSS: https://www.codecademy.com/catalog/language/html-css",
+        "questions":[
+           {
+             "question":"Who is considered to be the first computer programmer? (full name)",
+              "answer": "Ada Lovelace",
+              "info": "Ada Lovelace was a well-educated mathematician in the 1800s, a time period when women were rarely allowed into the academic world. She is well-known for her annotations about Charles Babbage's Analytical Machine. Her work led her to be considered the first computer programmer. In the 1970s, a programming language was named Ada in her honor.",
+             "link":"https://www.newyorker.com/tech/annals-of-technology/ada-lovelace-the-first-tech-visionary"
+           },
+           {
+              "question":"What is the simple code for printing \"Hello World\" in python?",
+              "answer":"print(\"Hello World\")",
+              "info": "Python is a newer programming language from 1991 and is most often used for data science and machine learning. It is a popular choice for beginners because it is easy to learn and has a large community.",
+             "link":"https://www.w3schools.com/python/python_intro.asp"
+           },
+           {
+              "question":"Convert the following integer to and 8-bit binary sequence: 84",
+              "answer":"01010100",
+              "info":"Binary is a base two number system, as opposed to our decimal system being base 10. Binary is the basis of all data stored in computers. Programming is used as a way to communicate between human-language and binary.",
+             "link":"https://www.geeksforgeeks.org/binary-number/"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletPur.png"]
+     },
+     "healthPlant":{
+        "type":"Pink Daisy",
+       "title": "Mental Health",
+        "level":0,
+        "unlockLevel": 3,
+       "info": "Here are some resources to help you with your mental health! General Link: https://www.nimh.nih.gov/health/find-help, Cook-Counseling: https://ucc.vt.edu/, Virginia Tech Resources: https://medicine.vtc.vt.edu/student-life/student-wellness/mental-health-resources.html",
+        "questions":[
+           {
+             // https://www.samhsa.gov/mental-health/myths-and-facts
+              "question":"One in _ Americans experience Mental Health in a Year. A:10 B: 5 C: 20 D: 8",
+              "answer":"B",
+             "info": "There are more americans that experience mental health in a year since people don't think that they have one and don't realize that they do!",
+           },
+           {
+             // https://mhanational.org/mental-health-treatments
+             "question":"How do people often get treatment? A: Medication B: Therapy C: Both",
+             "answer":"C"
+           },
+           {
+             // https://mhanational.org/mentalhealthfacts
+             "question":"About 42.5 Million Americans Have:",
+             "answer":"anxiety"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletPinkFlowers.png"]
+     },
+     "peoplePlant":{
+        "type":"Cactus",
+       "title": "Famous Women",
+        "level":0,
+       "unlockLevel":6,
+       "info":"Want to learn more about women impowerment? Check out these links",
+        "questions":[
+           {
+              "question":"Who was the women genius in Science that worked on Radioactivity and X-Rays? (first and last name)",
+              "answer":"Marie Curie",
+              "info":"Marie Curie was a Polish and naturalized-French physicist who was the first woman to win a Nobel Prize and the only woman to receive two Nobel Prizes. She is known for discovering both Uranium and Plutonium. Unfortunately, her exposure to Radium gave her aplastic pernicious anaemia and ultimately led to her death.",
+             "link":"/https://www.nobelprize.org/prizes/physics/1903/marie-curie/biographical/"
+           },
+           {             
+              "question":"Author of Uncle Tom's Cabin, a book widely popular right before the American Civil War, which pointed out the evils of slavery. (first, middle, and last names)",
+              "answer":"Harriet Beecher Stowe",
+             "info": "Harriet Beecher Stowe was an American writer best known for speaking out against slavery. As women did not yet have the right to vote, she argued for the abolition of slavery through her writing. Her book made a huge impact on the public opinions of individuals, especially women in northern states.",
+             "link": "https://www.womenshistory.org/education-resources/biographies/harriet-beecher-stowe"
+           },
+          {
+              "question":"Who was a strong advocate of women's suffrage as well as the founder of the Red Cross? (first and last name)",
+              "answer":"Florence Nightingale",
+              "info": "Florence Nigthingale helped people her entire life. Her promotion of cleanliness was a major help in the minimization of spreading diseases. She used donations to establish the Red Cross, which has only grown with time. She is also known for writing over 130,000 campaign letters during her later, bed-ridden years.",
+               "link":"https://www.redcross.org.uk/stories/health-and-social-care/health/how-florence-nightingale-influenced-the-red-cross"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletCactus.png"]
+     },
+    "sustainPlant":{
+        "type":"Yellow Tulip",
+      "title": "Sustainability",
+        "level":0,
+        "unlockLevel": 9,
+      "info": "Want to learn more about sustainability? Check out these links: ",
+        "questions":[
+           {
+             //https://sustainability.georgetown.edu/50-facts-for-50-years/
+              "question":"The United States is the 1st _ producing country in the world",
+              "answer":"trash"
+           },
+           {
+             //https://sustainability.georgetown.edu/50-facts-for-50-years/
+              "question":"Recycling one aluminmum can save enough enegery to run a TV for _ hours?",
+              "answer":"3",
+             
+                  //https://climate.nasa.gov/evidence/#:~:text=Global%20Temperature%20Is%20Rising,atmosphere%20and%20other%20human%20activities.
+             "question":"The planet's average temperature has gone up at least _ degrees in the last few years!",
+             "answer":"2"
+             
+           },
+           {
+             //https://www.sustainability.gov/#:~:text=The%20Office%20of%20the%20Federal,and%20climate%20resilient%20Federal%20operations.
+              "question":"What is the name of the position that in charge of policy on sustainability in the US Government?",
+              "answer":"Chief Sustainability Officer"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletYellowFlower.png"]
+     },
+     "financePlant":{
+         "type":"Orange Chrysanthemum",
+          "title": "Coding",
+         "level":0,
+         "unlockLevel":0,
+       "info": "Want to learn about getting finanical help, start with the stocks and more? Check this links!: Zero Waste Homes: https://zerowastehome.com/, Green Life Homes: https://inhabitat.com/",
+         "questions":[
             {
-                "question": "1?",
-                "answer": "1"
+              //https://educationdata.org/financial-aid-statistics
+               "question":"About what percentage of undergraduates get a loan?",
+               "answer":"37"
             },
             {
-                "question": "2?",
-                "answer": "2"
-            }
-        ]
-    },
-    {
-        "name": "testPlant",
-        "type": "flower",
-        "level": 0,
-        "questions": [
-            {
-                "question": "1?",
-                "answer": "1"
-            }
-        ]
-    }
-]
+              // https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8806009/
+               "question":"1 out of 5 people say that they could not afford: ",
+               "answer":"rent"
+            },
+           {
+             //https://www.bankrate.com/banking/savings/emergency-savings-report/
+              "question":"22% of Americans lack a _ fund?",
+              "answer":"emergency"
+           }
+         ],
+         "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletOrange.png"]
+      }
+   }
