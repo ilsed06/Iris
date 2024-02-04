19da25cfd86511e70e417ec4acb89c0b0e71c9ff
ilsed06 <130949186+ilsed06@users.noreply.github.com>
2024-02-04 03:45:53 -0500

    Update data.js
    even more data

--- data.js
+++ data.js
@@ -1,29 +1,126 @@
-const data = 
-[
-    {
-        "name": "codePlant",
-        "type": "iris",
-        "level": 0,
-        "questions": [
+const data =
+  {
+     "codePlant":{
+        "type":"Purple Iris",
+        "title": "Coding",
+        "level":0,
+        "unlockLevel": 0,
+       "info": "Want to learn more about coding? Check these links!",
+        "questions":[
+           {
+             "question":"Who is considered to be the first computer programmer? (full name)",
+              "answer": "Ada Lovelace",
+              "info": "Ada Lovelace was a well-educated mathematician in the 1800s, a time period when women were rarely allowed into the academic world. She is well-known for her annotations about Charles Babbage's Analytical Machine. Her work led her to be considered the first computer programmer. In the 1970s, a programming language was named Ada in her honor.",
+             "link":"https://www.newyorker.com/tech/annals-of-technology/ada-lovelace-the-first-tech-visionary"
+           },
+           {
+              "question":"What is the simple code for printing \"Hello World\" in python?",
+              "answer":"print(\"Hello World\")",
+              "info": "Python is a newer programming language from 1991 and is most often used for data science and machine learning. It is a popular choice for beginners because it is easy to learn and has a large community.",
+             "link":"https://www.w3schools.com/python/python_intro.asp"
+           },
+           {
+              "question":"Convert the following integer to and 8-bit binary sequence: 84",
+              "answer":"01010100",
+              "info":"Binary is a base two number system, as opposed to our decimal system being base 10. Binary is the basis of all data stored in computers. Programming is used as a way to communicate between human-language and binary.",
+             "link":"https://www.geeksforgeeks.org/binary-number/"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletPur.png"]
+     },
+     "healthPlant":{
+        "type":"Pink Daisy",
+       "title": "Mental Health",
+        "level":0,
+        "unlockLevel": 3,
+       "info": "Here are some resources to help you with your mental health!",
+        "questions":[
+           {
+             // https://www.samhsa.gov/mental-health/myths-and-facts
+              "question":"One in _ Americans experience Mental Health in a Year. A:10 B: 5 C: 20 D: 8",
+              "answer":"B"
+           },
+           {
+             // https://mhanational.org/mental-health-treatments
+             "question":"How do people often get treatment? A: Medication B: Therapy C: Both",
+             "answer":"C"
+           },
+           {
+             // https://mhanational.org/mentalhealthfacts
+             "question":"About 42.5 Million Americans Have:",
+             "answer":"anxiety"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletPinkFlowers.png"]
+     },
+     "peoplePlant":{
+        "type":"Cactus",
+       "title": "Famous Women",
+        "level":0,
+        "unlockLevel":6,
+        "questions":[
+           {
+              "question":"Who was the women genius in Science that worked on Radioactivity and X-Rays? (first and last name)",
+              "answer":"Marie Curie",
+              "info":"Marie Curie was a Polish and naturalized-French physicist who was the first woman to win a Nobel Prize and the only woman to receive two Nobel Prizes. She is known for discovering both Uranium and Plutonium. Unfortunately, her exposure to Radium gave her aplastic pernicious anaemia and ultimately led to her death.",
+             "link":"/https://www.nobelprize.org/prizes/physics/1903/marie-curie/biographical/"
+           },
+           {             
+              "question":"Author of Uncle Tom's Cabin, a book widely popular right before the American Civil War, which pointed out the evils of slavery. (first, middle, and last names)",
+              "answer":"Harriet Beecher Stowe",
+             "info": "Harriet Beecher Stowe was an American writer best known for speaking out against slavery. As women did not yet have the right to vote, she argued for the abolition of slavery through her writing. Her book made a huge impact on the public opinions of individuals, especially women in northern states.",
+             "link": "https://www.womenshistory.org/education-resources/biographies/harriet-beecher-stowe"
+           },
+          {
+              "question":"Who was a strong advocate of women's suffrage as well as the founder of the Red Cross? (first and last name)",
+              "answer":"Florence Nightingale",
+              "info": "Florence Nigthingale helped people her entire life. Her promotion of cleanliness was a major help in the minimization of spreading diseases. She used donations to establish the Red Cross, which has only grown with time. She is also known for writing over 130,000 campaign letters during her later, bed-ridden years.",
+               "link":"https://www.redcross.org.uk/stories/health-and-social-care/health/how-florence-nightingale-influenced-the-red-cross"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletCactus.png"]
+     },
+    "womenPlant":{
+        "type":"Yellow Tulip",
+        "level":0,
+        "unlockLevel": 9,
+        "questions":[
+           {
+              "question":"1?",
+              "answer":"1"
+           },
+           {
+              "question":"2?",
+              "answer":"2"
+           },
+           {
+              "question":"3?",
+              "answer":"3"
+           }
+        ],
+        "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletYellowFlower.png"]
+     },
+     "financePlant":{
+         "type":"Orange Chrysanthemum",
+          "title": "Coding",
+         "level":0,
+         "unlockLevel":0,
+       "info": "Want to learn about getting finanical help, start with the stocking",
+         "questions":[
             {
-                "question": "1?",
-                "answer": "1"
+               "question":"1?",
+               "answer":"1"
             },
             {
-                "question": "2?",
-                "answer": "2"
-            }
-        ]
-    },
-    {
-        "name": "testPlant",
-        "type": "flower",
-        "level": 0,
-        "questions": [
-            {
-                "question": "1?",
-                "answer": "1"
-            }
-        ]
-    }
-]
+              // https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8806009/
+               "question":"1 out of 5 people say that they could not afford: ",
+               "answer":"rent"
+            },
+           {
+              "question":"3?",
+              "answer":"3"
+           }
+         ],
+         "images":["HackVioletPot.png", "HackVioletSapling.png", "HackVioletOrange.png"]
+      }
+   }
